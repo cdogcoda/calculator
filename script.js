cb03e4cbe456c2460dcbbe8c8ace2f036c8befc9
CDog <cdog97456@gmail.com>
2024-03-25 20:39:16 -0700

    Create object of basic operations (functions)

---
+++ script.js
@@ -0,0 +1,17 @@
+// TOP's pseudocode steps
+// Create functions that will represent each of the basic operations in the calculator
+// -> These operations are functions like adding, subtracting, etc
+// Create a function operate that takes an operator {string} and two numbers {number} as input
+// -> The function should be able to call to the set of operation functions that we create
+// Create the UI for the calculator
+// -> No need to connect JS to the setup yet
+// -> Fill up the calculator with dummy numbers and a clear button to ensure it looks right
+// Create a function to display the result of a calculation
+// Assign an event listener to the equals button such that when clicked, it calls the function operate and displays the result
+
+let operationsObject = {
+    "+": (firstNum, secondNum) => firstNum + secondNum,
+    "-": (firstNum, secondNum) => firstNum - secondNum,
+    "*": (firstNum, secondNum) => firstNum * secondNum,
+    "/": (firstNum, secondNum) => firstNum / secondNum,
+}
